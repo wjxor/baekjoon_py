e0579898b583701da6e8b85aa4373c47f71fd8ec
wjxor <shko940575@gmail.com>
2023-10-09 05:18:34 +0900

    2525

--- practice.py
+++ practice.py
@@ -1,3 +1,13 @@
-a, b, c = map(int, input().split())
+H, M = map(int, input().split())
+time = int(input())
 
-print(a + b + c)
+H += time // 60
+M += time % 60
+
+if (M >= 60) :
+    H += 1
+    M -= 60
+if (H >= 24) :
+    H -= 24
+
+print(H, M)
