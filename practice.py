adf087c7ceb4f333c4fc014b3e7f486e1e468b53
wjxor <shko940575@gmail.com>
2024-02-24 17:05:02 +0900

    2562

--- practice.py
+++ practice.py
@@ -1,12 +1,8 @@
-N = int(input())
-A = list(map(int, input().split()))
-max = A[0]
-min = A[0]
+list = []
 
-for i in range(N):
-    if (A[i] > max):
-        max = A[i]
-    elif (A[i] < min):
-        min = A[i]
-        
-print(min, max)
+for i in range(9):
+    list.append(int(input()))
+
+
+print(max(list))
+print(list.index(max(list)) + 1)
