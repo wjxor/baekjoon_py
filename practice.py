8956ce991ac0501fce20108d926e99496f3e17ce
wjxor <shko940575@gmail.com>
2024-02-24 16:48:19 +0900

    10818

--- practice.py
+++ practice.py
@@ -1,6 +1,12 @@
-N, X = map(int, input().split())
+N = int(input())
 A = list(map(int, input().split()))
+max = A[0]
+min = A[0]
 
 for i in range(N):
-    if (A[i] < X):
-        print(f"{A[i]}", end=" ")
+    if (A[i] > max):
+        max = A[i]
+    elif (A[i] < min):
+        min = A[i]
+        
+print(min, max)
