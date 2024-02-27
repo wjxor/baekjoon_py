2f71e049fb0b565b43145460e2f7b0e61aae270a
wjxor <shko940575@gmail.com>
2024-02-27 20:27:42 +0900

    10813

--- practice.py
+++ practice.py
@@ -1,14 +1,16 @@
 N, M = map(int, input().split())
-# list 선언 후 0으로 초기화
-basket = [0 for _ in range(N)]
+# list 선언 후 각 번호에 해당되는 공 넣기
+basket = [i for i in range(1, N + 1)]
+temp = 0
 
-# M번동안 i, j, k의 값을 받기위해 for loop
-for _ in range(M):
-    i, j, k = map(int, input().split())
-    # i ~ j까지이기에 (i, j + 1)
-    # list는 0번부터 시작하므로 -1
-    for n in range(i, j + 1):
-        basket[n - 1] = k
+for i in range(M):
+    i, j = map(int, input().split())
 
-for n in range(N):
-    print(basket[n], end = ' ')
+    # 교환 시작
+    temp = basket[i - 1]
+    basket[i - 1] = basket[j - 1]
+    basket[j - 1] = temp
+    # 교환 끝
+
+for i in range(N):
+    print(basket[i], end=' ')
