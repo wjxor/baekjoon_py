a1f8a40fee8be003710a19a1ba9112554d9f765f
wjxor <shko940575@gmail.com>
2024-03-28 21:11:49 +0900

    1546

--- practice.py
+++ practice.py
@@ -1,15 +1,9 @@
-N, M = map(int, input().split())
+N = int(input())
 
-# 바구니에 번호 부여
-basket = [n for n in range(1, N + 1)]
+scores = list(map(int, input().split()))
+M = max(scores)
 
-for _ in range(M):
-    i, j = map(int , input().split())
-    
-    #뒤바꾸기 위해 temp에 보관
-    temp = basket[i - 1:j]
-    temp.reverse()
-    basket[i -1:j] = temp
+for i in range(N):
+    scores[i] = scores[i]/M*100
 
-for i in range (N):
-    print(basket[i], end=" ")
+print(sum(scores)/N)
